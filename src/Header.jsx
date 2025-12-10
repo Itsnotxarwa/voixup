9f556082ad97ea2416aef17d8b9c7dff113d212d
itsnotxarwa <marwa.kchkh@gmail.com>
2025-12-10 13:39:24 +0100

    fix header bg

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
 
     return(
-        <header className="sticky top-0 z-50 border-b border-black/10 backdrop-blur-xl bg-white/80 dark:bg-[#102A43]">
+        <header className="sticky top-0 z-50 border-b border-black/10 backdrop-blur-xl bg-white/80">
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                         <div className="flex justify-between items-center py-4">
                             <Link to="/">
